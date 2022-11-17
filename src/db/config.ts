c90d05409bfe229c52eac2569acf93ab6fb30a11
Murod <almatov.ms@gmail.com>
2022-11-17 16:21:47 +0500

    create db if not exists

--- src/db/config.ts
+++ src/db/config.ts
@@ -1,5 +1,6 @@
-import config from "../config";
 import { Dialect, Sequelize } from "sequelize";
+import * as mysql from "mysql2/promise";
+import config from "../config";
 
 const sequelize = new Sequelize(
     config.database.name,
@@ -11,4 +12,10 @@ const sequelize = new Sequelize(
     }
 );
 
-export { sequelize };
+async function createDb() {
+    const {host, user, password, name} = config.database;
+    const connection = await mysql.createConnection({ host, user, password });
+    await connection.query(`CREATE DATABASE IF NOT EXISTS \`${name}\`;`);
+}
+
+export { sequelize, createDb };
--- src/db/init.ts
+++ src/db/init.ts
@@ -1,8 +1,14 @@
 import { File, User } from "./models";
+import { createDb } from "./config";
 
-const dbInit = () => Promise.all([
-    User.sync({ alter: true }),
-    File.sync({ alter: true })
-]);
+// don't use this func in production
+const dbInit = async () => {
+    await createDb();
+
+    await Promise.all([
+        User.sync({ alter: true }),
+        File.sync({ alter: true })
+    ]);
+}
 
 export default dbInit;
