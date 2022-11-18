bee6a3a952130eb23308560730e27b2ccd34160a
Murod <almatov.ms@gmail.com>
2022-11-18 20:55:53 +0500

    file upload logic

--- src/routes/file.ts
+++ src/routes/file.ts
@@ -1,12 +1,21 @@
 import express from "express";
+import uploadRouter from "./file/upload";
+import fileUpload from "express-fileupload";
 
 const router = express.Router();
 
-router.post("/upload");
+const opt = {
+    createParentPath: true,
+    limits: { files: 1 },
+    useTempFiles: true,
+    tempFileDir: "files_tmp/"
+}
+
+router.post("/upload", fileUpload(opt), uploadRouter);
 router.get("/list");
 router.delete("/delete/:id");
 router.get("/:id");
 router.get("/download/:id");
-router.put("/update/:id");
+router.put("/update/:id", fileUpload(opt));
 
 export default router;
---
+++ src/routes/file/upload.ts
@@ -0,0 +1,37 @@
+import { NextFunction, Response } from "express";
+import { Request } from "express-jwt";
+import path from "path";
+import { UploadedFile } from "express-fileupload";
+import * as fileService from "../../db/services/file-service";
+
+export default async function upload(req: Request, res: Response, next: NextFunction) {
+    if (!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400).send('No files were uploaded.');
+    }
+    const fileInfo = req.files["fileUpload"] as UploadedFile;
+
+    const { id: userId } = req.auth;
+
+    const extDot = path.extname(fileInfo.name);
+    const ext = extDot.startsWith(".") ? extDot.substring(1) : "";
+    const basename = path.basename(fileInfo.name, extDot);
+
+    const file = await fileService.create({
+        user_id: userId,
+        name: basename,
+        extension: ext,
+        mime: fileInfo.mimetype,
+        size: fileInfo.size,
+        date_upload: new Date()
+    });
+
+    const filePath = path.join("files", file.id.toString());
+    fileInfo.mv(filePath, function (err) {
+        if (err) {
+            return next(err);
+        }
+
+        return res.status(200).send("OK");
+    });
+}
+
